8c3cc10976e5a2ed2851b2e02d070d68f2b4a40b
Ehi Nosakhare <ehnosakh@microsoft.com>
2019-12-16 12:33:10 -0500

    fixed regularization parameter error. Added funtionality explain classification layer

--- python/interpret_text/msra/MSRAExplainer.py
+++ python/interpret_text/msra/MSRAExplainer.py
@@ -12,15 +12,21 @@ from pytorch_pretrained_bert import BertTokenizer
 from interpret_text.common.structured_model_mixin import PureStructuredModelMixin
 from interpret_text.explanation.explanation import _create_local_explanation
 
+from utils_msra import get_embedding, make_bert_embeddings
+
 
 class MSRAExplainer(PureStructuredModelMixin, nn.Module):
     """The MSRAExplainer for returning explanations for pytorch NN models.
     """
 
-    def __init__(self, device):
+    def __init__(self, device, target_layer=12, total_layers=12):
         """ Initialize the MSRAExplainer
         :param device: A pytorch device
         :type device: torch.device
+        :param target_layer: The target layer to explain. Default is 14, which is the classification layer. if set to -1, all layers will be explained
+        :type target_layer: int
+        :param total_layer: The total number of bert hidden layers
+        :type total_layer: int
         """
         super(MSRAExplainer, self).__init__()
         self.device = device
@@ -32,9 +38,12 @@ class MSRAExplainer(PureStructuredModelMixin, nn.Module):
         # Phi: A function whose input is x (element in the first parameter) and returns a hidden state (of type ``torch.FloatTensor``, of any shape
         self.Phi = None
         self.regular = None
+        self.target_layer = target_layer
+        self.total_layers = total_layers
 
         assert self.scale >= 0, "the value for scale cannot be less than zero"
         assert 1 >= self.rate >= 0, "the value for rate has to be between 0 and 1"
+        assert type(self.target_layer) == int and (self.target_layer == -1 or 1 <= self.target_layer <= 14), "the value of the target layer has to be an interger and either -1 (all bert layers), or 1 to 12 (specific bert layer) or 13 (pooler layer), or 14 (final classification layer). You want to  you want to access layer %d" % (self.target_layer)
 
     def explain_local(self, model, embedded_input, regularization, dataset=None):
         """Explain the model by using MSRA's interpretor
@@ -49,21 +58,25 @@ class MSRAExplainer(PureStructuredModelMixin, nn.Module):
         :return: A model explanation object. It is guaranteed to be a LocalExplanation
         :rtype: DynamicLocalExplanation
         """
+        self.input_embeddings = embedded_input
+        
         assert self.input_embeddings is not None, "input embeddings are required to generate explanation"
         
-        self.input_embeddings = embedded_input
+        
         self.input_size = self.input_embeddings.size(0)
         self.input_dimension = self.input_embeddings.size(1)
         self.ratio = nn.Parameter(torch.randn(self.input_size, 1), requires_grad=True)
-        self.regular = regularization
+        self.input_embeddings.to(self.device)
+        #self.regular = regularization
 
-        if self.regular is not None:
-            self.regular = nn.Parameter(torch.tensor(self.regular).to(self.input_embeddings), requires_grad=False)
-            self.Phi = self.generate_Phi(model, 3)
+        if regularization is not None:
+            self.regular = nn.Parameter(torch.tensor(regularization).to(self.input_embeddings), requires_grad=False)
+            self.Phi = self.generate_Phi(model, self.target_layer, self.total_layers)
         else:
-            assert dataset is not None, "Dataset is required if explainer not initialized with regularization parameter"
-            self.Phi = self.generate_Phi(model, 3)
-            self.regular = self._calculate_regularization(dataset, self.Phi)
+            assert dataset is not None, "Training dataset is required if explainer not initialized with regularization parameter"
+            self.regular = self._calculate_regularization(dataset, self.Phi) #TODO: fix this
+            self.Phi = self.generate_Phi(model, self.target_layer, self.total_layers)
+            
 
         # values below are arbitarily set for now
         self.optimize(iteration=5000, lr=0.01, show_progress=True)
@@ -71,7 +84,7 @@ class MSRAExplainer(PureStructuredModelMixin, nn.Module):
         self.local_importance_values = local_importance_values
         return _create_local_explanation(local_importance_values=np.array(local_importance_values), method="neural network", model_task="classification")
 
-    def getRegularizationBERT(self, model, explain_layer=3):
+    def getRegularizationBERT(self, model):
         """
         :param model: A pytorch model
         :type model: torch.nn
@@ -91,10 +104,10 @@ class MSRAExplainer(PureStructuredModelMixin, nn.Module):
                     raise Exception("Too many failed HTTP Requests")
                 print("Request Failed, trying again...")
         regularization_bert = json.loads(data)
-        self.Phi = self.generate_Phi(model, layer=explain_layer)
+        self.Phi = self.generate_Phi(model, layer=self.target_layer, total_layers=self.total_layers)
         return regularization_bert
 
-    def calculate_regularization(self, sampled_x, model, device, explain_layer=3, reduced_axes=None):
+    def calculate_regularization(self, sampled_x, model, reduced_axes=None):
         """ Calculate the variance of the state generated from the perturbed inputs that is used for Interpreter
         :param sampled_x: A list of sampled input embeddings $x$, each $x$ is of shape ``[length, dimension]``. All the $x$s can have different length, but should have the same dimension. Sampled number should be higher to get a good estimation.
         :type sampled_x: list[torch.Tensor]
@@ -113,18 +126,24 @@ class MSRAExplainer(PureStructuredModelMixin, nn.Module):
         """
         sample_num = len(sampled_x)
         sample_s = []
-        self.Phi = self.generate_Phi(model, layer=explain_layer)
+        self.Phi = self.generate_Phi(model, layer=self.target_layer, total_layers=self.total_layers)
         for n in range(sample_num):
             x = sampled_x[n]
-            if device is not None:
-                x = x.to(device)
+            #print(np.shape(x))
+            if self.device is not None:
+                x = x.to(self.device)
+            
             s = self.Phi(x)
+            if n==1: 
+                print(s)
+                print(np.shape(s))
             if reduced_axes is not None:
                 for axis in reduced_axes:
                     assert axis < len(s.shape)
                     s = s.mean(dim=axis, keepdim=True)
             sample_s.append(s.tolist())
         sample_s = np.array(sample_s)
+        #print(np.shape(sample_s))
         return np.std(sample_s, axis=0)
 
     def forward(self):
@@ -135,8 +154,8 @@ class MSRAExplainer(PureStructuredModelMixin, nn.Module):
         :rtype: torch.FloatTensor
         """
         ratios = torch.sigmoid(self.ratio)  # S * 1
-        x = self.input_embeddings + 0.0  # S * D
-        x_tilde = x + ratios * torch.randn(self.input_size, self.input_dimension).to(x.device) * self.scale  # S * D
+        x = self.input_embeddings +  0.0 #torch.zeros(ratios.shape).to(self.device)#0.0  # S * D
+        x_tilde = x + ratios * torch.randn(self.input_size, self.input_dimension).to(x.device) * self.scale # S * D
         s = self.Phi(x)  # D or S * D
         s_tilde = self.Phi(x_tilde)
         loss = (s_tilde - s) ** 2
@@ -161,16 +180,20 @@ class MSRAExplainer(PureStructuredModelMixin, nn.Module):
         minLoss = None
         state_dict = None
         optimizer = optim.Adam(self.parameters(), lr=lr)
+        
         self.train()
         func = (lambda x: x) if not show_progress else tqdm
         for _ in func(range(iteration)):
             optimizer.zero_grad()
+            if self.device is not None:
+                self.to(self.device)
             loss = self()
             loss.backward(retain_graph=True)
             optimizer.step()
             if minLoss is None or minLoss > loss:
                 state_dict = {k: self.state_dict()[k] + 0.0 for k in self.state_dict().keys()}
                 minLoss = loss
+        
         self.eval()
         self.load_state_dict(state_dict)
 
@@ -182,6 +205,54 @@ class MSRAExplainer(PureStructuredModelMixin, nn.Module):
         ratios = torch.sigmoid(self.ratio)  # S * 1
         return ratios.detach().cpu().numpy()[:, 0] * self.scale
 
+    def generate_Phi(self, model, layer, total_layers):
+        """Generate the Phi/hidden state that needs to be interpreted
+        /Generates a function that returns the new hidden state given a new perturbed input is passed
+        :param model: a pytorch model
+        :type: torch.nn
+        :param layer: the layer to generate Phi for
+        :type layer: int
+        :param total_layers: the total number of bert layers
+        :return: The phi function
+        :rtype: function
+        """
+        # # Assuming below that model has only 12 layers, will change this later
+        # #changed
+        # assert (
+        #     1 <= layer <= 12
+        # ), "model only has 12 layers, while you want to access layer %d" % (layer)
+
+        def Phi(x):
+            x = x.unsqueeze(0)
+            attention_mask = torch.ones(x.shape[:2]).to(x.device)
+            extended_attention_mask = attention_mask.unsqueeze(1).unsqueeze(2)
+            extended_attention_mask = extended_attention_mask.to(dtype=torch.float)
+            extended_attention_mask = (1.0 - extended_attention_mask) * -10000.0
+            
+            hidden_states = x
+
+            #print('This is the layer num', layer)
+
+            # if layer==13:
+            # #explain the pooler
+            #     print('not yet implemented')
+               
+            
+            if layer == 14:
+            #explain the classification layer
+                encoder = model.bert.encoder
+                pooler = model.bert.pooler
+                classifier = model.classifier 
+                return classifier(pooler(encoder(hidden_states, attention_mask, False)[-1]))[0]
+
+            # extract one of the bert layers
+            model_list = model.bert.encoder.layer[:layer]
+            
+            for layer_module in model_list:
+                hidden_states = layer_module(hidden_states, extended_attention_mask)
+            return hidden_states[0][layer]
+        return Phi
+        
     def visualize(self, text, max_alpha=0.5):
         """ Currently a placeholder visualize function until python widget is in a working state
         :param text: sample text
@@ -220,35 +291,6 @@ class MSRAExplainer(PureStructuredModelMixin, nn.Module):
         highlighted_text = highlighted_text[1:-2]
         highlighted_text = ' '.join(highlighted_text)
         display(HTML(highlighted_text))
-
-    def generate_Phi(self, model, layer):
-        """Generate the Phi/hidden state that needs to be interpreted
-        /Generates a function that returns the new hidden state given a new perturbed input is passed
-        :param model: a pytorch model
-        :type: torch.nn
-        :param layer: the layer to generate Phi for
-        :type layer: int
-        :return: The phi function
-        :rtype: function
-        """
-        # Assuming below that model has only 12 layers, will change this later
-        assert (
-            1 <= layer <= 12
-        ), "model only has 12 layers, while you want to access layer %d" % (layer)
-
-        def Phi(x):
-            x = x.unsqueeze(0)
-            attention_mask = torch.ones(x.shape[:2]).to(x.device)
-            extended_attention_mask = attention_mask.unsqueeze(1).unsqueeze(2)
-            extended_attention_mask = extended_attention_mask.to(dtype=torch.float)
-            extended_attention_mask = (1.0 - extended_attention_mask) * -10000.0
-            # extract the 3rd layer
-            model_list = model.encoder.layer[:layer]
-            hidden_states = x
-            for layer_module in model_list:
-                hidden_states = layer_module(hidden_states, extended_attention_mask)
-            return hidden_states[0]
-        return Phi
 
     def plot_global_imp(self, top_words, top_importances, label_name):
         plt.figure(figsize=(8,4))
