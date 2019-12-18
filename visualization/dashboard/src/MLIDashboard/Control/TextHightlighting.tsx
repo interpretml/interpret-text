558aecc0c18fb4907ab3485e77d78d860dc9491b
Rebecca Jiang <rejia@microsoft.com>
2019-12-18 11:43:07 -0500

    simplified argsort

--- visualization/dashboard/src/MLIDashboard/Control/TextHightlighting.tsx
+++ visualization/dashboard/src/MLIDashboard/Control/TextHightlighting.tsx
@@ -20,7 +20,7 @@ export class TextHighlighting extends React.PureComponent<IDatasetSummary> {
     const text = this.props.text
     const importance = this.props.localExplanations
     const k = this.props.topK
-    const sortedList = this.argsort(importance).splice(0, k)
+    const sortedList = this.argsort(importance.map(Math.abs)).reverse().splice(0,k)
     return text.map((word, wordIndex) => {
       let styleType:any
       const score = importance[wordIndex]
@@ -34,29 +34,11 @@ export class TextHighlighting extends React.PureComponent<IDatasetSummary> {
       return <span style = {styleType} title = {score.toString()}>{word + ' '}</span>
     })
   }
-
-  public argsort (toSort:any[], direction?:string) {
-    const sortedList = []
-    for (const i in toSort) {
-      sortedList.push([toSort[i], parseInt(i)])
-    }
-    if (typeof direction === 'undefined') {
-      sortedList.sort(function (left, right) {
-        return Math.abs(left[0]) > Math.abs(right[0]) ? -1 : 1
-      })
-    } else if (direction === 'positive') {
-      sortedList.sort(function (left, right) {
-        return left[0] > right[0] ? -1 : 1
-      })
-    } else if (direction === 'negative') {
-      sortedList.sort(function (left, right) {
-        return left[0] < right[0] ? -1 : 1
-      })
-    }
-    const returnList = []
-    for (const i in sortedList) {
-      returnList.push(sortedList[i][1])
-    }
-    return returnList
-  }
+  public argsort(toSort: number[]): number[] {
+    const sorted = toSort.map((val, index) => [val, index]);
+      sorted.sort((a, b) => {
+          return a[0] - b[0];
+      });
+      return sorted.map(val => val[1]);
+     }
 }
