eac1b7a57f1d2c1b9e9fa22aca237e0cc97b454b
rebeccaj1211 <rejia@microsoft.com>
2019-12-18 11:46:48 -0500

    sliding bar with top k overall features working (#24)
    * sliding bar with top k overall features working with small fixes to text highlighting
    *slider max is the number of important words, not all words

--- visualization/dashboard/src/MLIDashboard/ExplanationDashboard.tsx
+++ visualization/dashboard/src/MLIDashboard/ExplanationDashboard.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { IExplanationDashboardProps } from './Interfaces/IExplanationDashboardProps'
 import { TextHighlighting } from './Control/TextHightlighting'
 import { localization } from '../Localization/localization'
+import { Slider } from 'office-ui-fabric-react/lib/Slider'
 
 export interface IDashboardContext {
 }
@@ -10,17 +11,48 @@ export interface IDashboardState {
 }
 
 export class ExplanationDashboard extends React.PureComponent<IExplanationDashboardProps, IDashboardState> {
+  state = {
+    maxK: this.countNonzeros(this.props.dataSummary.localExplanations),
+    topK: Math.ceil(this.countNonzeros(this.props.dataSummary.localExplanations) / 2)
+  }
+
   public render () {
     return (
       <>
         <h1>{localization.interpretibilityDashboard}</h1>
-        <TextHighlighting
-          text = {this.props.dataSummary.text}
-          localExplanations = {this.props.dataSummary.localExplanations}
-          classNames = {this.props.dataSummary.classNames}
-          prediction = {this.props.dataSummary.prediction}
-        />
+        <div className = "explainerDashboard">
+          <Slider
+            label={this.state.topK.toString().concat(' ', localization.importantWords)}
+            min={1}
+            max={this.state.maxK}
+            step={1}
+            defaultValue={(this.state.topK)}
+            showValue={true}
+            onChange={(value) => this.setTopK(value)}
+          />
+          <TextHighlighting
+            text = {this.props.dataSummary.text}
+            localExplanations = {this.props.dataSummary.localExplanations}
+            classNames = {this.props.dataSummary.classNames}
+            prediction = {this.props.dataSummary.prediction}
+            topK = {this.state.topK}
+          />
+        </div>
       </>
     )
+  }
+
+  private setTopK (newNumber: number):void{
+    this.setState({ topK: newNumber })
+  }
+
+  private countNonzeros (numArr: number[]):number {
+    let counter = 0
+    for (const i in numArr) {
+      if (numArr[i] !== 0) {
+        counter++
+      }
+    }
+    return counter
   }
 }
