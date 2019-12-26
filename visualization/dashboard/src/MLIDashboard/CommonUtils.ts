1f49ce52445dfe64bf5d416cb7b5315682e62732
Rebecca Jiang <rejia@microsoft.com>
2019-12-26 15:14:52 -0500

    changed indentation on commonutils

--- visualization/dashboard/src/MLIDashboard/CommonUtils.ts
+++ visualization/dashboard/src/MLIDashboard/CommonUtils.ts
@@ -1,9 +1,9 @@
-export class Utils{
-    public static argsort(toSort: number[]): number[] {
-        const sorted = toSort.map((val, index) => [val, index]);
-        sorted.sort((a, b) => {
-            return a[0] - b[0];
-        });
-        return sorted.map(val => val[1]);
-        }
+export class Utils {
+  public static argsort (toSort: number[]): number[] {
+    const sorted = toSort.map((val, index) => [val, index])
+    sorted.sort((a, b) => {
+      return a[0] - b[0]
+    })
+    return sorted.map(val => val[1])
+  }
 }
