cdb60914a09c927f26c5bdf22766e2abaada19d4
Walter Martin <wamartin@microsoft.com>
2020-01-06 13:00:12 -0500

    other changes to text

--- python/interpret_text/__init__.py
+++ python/interpret_text/__init__.py
@@ -3,15 +3,15 @@ import logging
 import os
 import atexit
 # Only log to disk if environment variable specified
-interpret_c_logs = os.environ.get('INTERPRET_TEXT_LOGS')
-if interpret_c_logs is not None:
+interpret_text_logs = os.environ.get('INTERPRET_TEXT_LOGS')
+if interpret_text_logs is not None:
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)
-    os.makedirs(os.path.dirname(interpret_c_logs), exist_ok=True)
-    handler = logging.FileHandler(interpret_c_logs, mode='w')
+    os.makedirs(os.path.dirname(interpret_text_logs), exist_ok=True)
+    handler = logging.FileHandler(interpret_text_logs, mode='w')
     handler.setLevel(logging.INFO)
     logger.addHandler(handler)
-    logger.info('Initializing logging file for interpret-community')
+    logger.info('Initializing logging file for interpret-text')
 
     def close_handler():
         handler.close()
