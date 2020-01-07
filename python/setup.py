c3d25a45dc70195507f5dcd1f21b8a877b8b089b
chhetri22 <abishkarchhetri@gmail.com>
2020-01-07 16:15:25 -0500

    Increase version of package

--- python/setup.py
+++ python/setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 import os
 
 _major = "0.1"
-_minor = "0.0"
+_minor = "0.1"
 
 if os.path.exists("../major.version"):
     with open("../major.version", "rt") as bf:
