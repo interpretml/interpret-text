da4e0fd327fbc77a27951fd24aa0429d19208c31
Rebecca Jiang <rejia@microsoft.com>
2020-01-15 13:36:17 -0500

    added fixes

--- visualization/dashboard/src/MLIDashboard/ExplanationDashboard.tsx
+++ visualization/dashboard/src/MLIDashboard/ExplanationDashboard.tsx
@@ -7,14 +7,19 @@ import {BarChart} from './Control/BarChart'
 import {Toggle} from 'office-ui-fabric-react/lib/Toggle';
 import {Text} from 'office-ui-fabric-react/lib/Text';
 import { Utils } from './CommonUtils'
+import { ChoiceGroup, IChoiceGroupOption } from 'office-ui-fabric-react/lib/ChoiceGroup';
 export interface IDashboardContext {}
 
 export interface IDashboardState {
   maxK: number,
   topK: number,
-  posToggle: boolean,
-  negToggle: boolean
+  radio: string
 }
+const options: IChoiceGroupOption[] = [
+  { key: 'all', text: localization.allButton },
+  { key: 'pos', text: localization.posButton },
+  { key: 'neg', text: localization.negButton}
+]
 
 export class ExplanationDashboard extends React.PureComponent<IExplanationDashboardProps, IDashboardState> {
   constructor(props: IExplanationDashboardProps, IDashboardState) {
@@ -22,11 +27,9 @@ export class ExplanationDashboard extends React.PureComponent<IExplanationDashbo
     this.state = {
       maxK: this.countNonzeros(this.props.dataSummary.localExplanations),
       topK: Math.ceil(this.countNonzeros(this.props.dataSummary.localExplanations) / 2),
-      posToggle: false,
-      negToggle: false
+      radio: "all"
     }
-    this.setPosToggle = this.setPosToggle.bind(this)
-    this.setNegToggle = this.setNegToggle.bind(this)
+    this.changeRadioButton = this.changeRadioButton.bind(this)
   }
 
 
@@ -38,7 +41,6 @@ export class ExplanationDashboard extends React.PureComponent<IExplanationDashbo
             <div className="ms-Grid-row">
               <div className="ms-Grid-col ms-sm6 ms-md6 ms-lg6">
                 <div style={{marginRight: '5px'}}>
-                  <div style={{fontFamily: 'Segoe UI', fontSize: '2.0em', fontWeight: "bold"}}>{localization.userText}</div>
                   <div style={{marginTop: '5%'}}>
                     <Slider
                       min={1}
@@ -55,44 +57,35 @@ export class ExplanationDashboard extends React.PureComponent<IExplanationDashbo
                       text={this.props.dataSummary.text}
                       localExplanations={this.props.dataSummary.localExplanations}
                       topK={this.state.topK}
-                      posOnly={this.state.posToggle}
-                      negOnly={this.state.negToggle}
+                      radio={this.state.radio}
                     />
-                    </div> 
+                    </div>
                     <div style={{fontFamily: 'Segoe UI', fontSize: '2.0em', fontWeight: "bold", marginBottom:'20px'}}>{localization.label + localization.colon + this.predictClass(this.props.dataSummary.classNames, this.props.dataSummary.prediction)}</div>
                     <div className="ms-Grid-col ms-sm6 ms-md6 ms-lg6" >
-                          <Toggle label={localization.posToggle} inlineLabel onChange={this.setPosToggle} checked={this.state.posToggle} />
-                          <Toggle label={localization.negToggle} inlineLabel onChange={this.setNegToggle} checked={this.state.negToggle} />
-                        </div>
-                    <div className="ms-Grid-col ms-sm6 ms-md6 ms-lg6" >
-                      {localization.legendText}
+                      <ChoiceGroup defaultSelectedKey="all" options={options} onChange={this.changeRadioButton} required={true} />
                     </div>
+                    <div className="ms-Grid-col ms-sm6 ms-md6 ms-lg6" >
+                          {localization.legendText}
+                        </div>
                 </div>
               </div>
               <div className="ms-Grid-col ms-sm6 ms-md6 ms-lg6" style={{padding:'20px',}} >
                 <div style={{marginLeft: '5px',}}>
-                  
                   <div style={{borderTopStyle: "solid", borderColor: 'rgb(0,120,212)', backgroundColor: 'white'}} >
-                    <div style={{backgroundColor: 'white', borderStyle: 'groove', borderBlockColor: 'black', borderRadius: '5px', height: '200px', padding:'20px', overflowY: 'auto'}}>
-                    <TextHighlighting
-                      text={this.props.dataSummary.text}
-                      localExplanations={this.props.dataSummary.localExplanations}
-                      topK={this.state.topK}
-                      posOnly={this.state.posToggle}
-                      negOnly={this.state.negToggle}
-                    />
+                    <div style={{backgroundColor: 'white', borderStyle: 'groove', borderBlockColor: 'black', borderRadius: '5px', height: '250px', padding:'20px', overflowY: 'auto'}}>
+                      <TextHighlighting
+                        text={this.props.dataSummary.text}
+                        localExplanations={this.props.dataSummary.localExplanations}
+                        topK={this.state.topK}
+                        radio={this.state.radio}
+                      />
                   </div>
                   <div style={{margin: '30px 10px 10px 10px'}}>
                     {localization.featureLegend}
                   </div>
                   <span style={{color: 'white', fontWeight: 'bold', backgroundColor: '#0078D4', margin: '10px'}}>{localization.posFeatureImportance}</span>
                   <br></br>
-                  <span style={{margin: '10px', fontWeight: 'bold', textDecorationLine: 'underline', color: '#0078D4'}}>{localization.negFeatureImportance}</span>
-                    <div className="ms-Grid" dir="ltr" >
-                      <div className="ms-Grid-row" style={{margin: '5%'}}>
-                      </div>
-                    </div>
-                    
+                  <span style={{margin: '10px', fontWeight: 'bold', textDecorationLine: 'underline', color: '#0078D4'}}>{localization.negFeatureImportance}</span>                   
                   </div>
                 </div>
               </div>
@@ -102,12 +95,9 @@ export class ExplanationDashboard extends React.PureComponent<IExplanationDashbo
       </div>
     )
 }
-
   private setTopK(newNumber: number): void {
     this.setState({topK: newNumber})
   }
-
-
   private countNonzeros(numArr: number[]): number {
     let counter = 0
     for (const i in numArr) {
@@ -117,16 +107,10 @@ export class ExplanationDashboard extends React.PureComponent<IExplanationDashbo
 }
     return counter
   }
-  public setPosToggle(ev: React.MouseEvent<HTMLElement>, checked: boolean) {
-    this.setState({posToggle: !this.state.posToggle, negToggle: false})
-  }
-  public setNegToggle() {
-    this.setState({negToggle: !this.state.negToggle, posToggle: false})
-  }
   public predictClass(classname, prediciton):string{
-    console.log(classname)
-    console.log("000")
-    console.log(prediciton)
     return classname[Utils.argsort(prediciton)[0]]
+  }
+  public changeRadioButton(ev: React.FormEvent<HTMLInputElement>, option: IChoiceGroupOption): void {
+    this.setState({radio: option.key})
   }
 }
