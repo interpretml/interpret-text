4a9e371c55fbe0d2df149d7e4652bb717d0049b9
Abishkar Chhetri <32040154+chhetri22@users.noreply.github.com>
2020-01-24 14:21:56 -0500

    Revert "fixing merges" commit (#47)
    This reverts commit 7cad5b121bacfa8cc3f0c42f6a1991e7e6ba1581, reversing
    changes made to dfb4a24f8e66103b7ebb1dc633066e8d4311ca77.

--- visualization/dashboard/src/MLIDashboard/ExplanationDashboard.tsx
+++ visualization/dashboard/src/MLIDashboard/ExplanationDashboard.tsx
@@ -19,11 +19,6 @@ export interface IDashboardState {
   topK: number,
   radio: string
 }
-const options: IChoiceGroupOption[] = [
-  { key: 'all', text: localization.allButton },
-  { key: 'pos', text: localization.posButton },
-  { key: 'neg', text: localization.negButton}
-]
 
 const options: IChoiceGroupOption[] = [
   { key: RadioKeys.all, text: localization.allButton },
@@ -37,72 +32,13 @@ export class ExplanationDashboard extends React.PureComponent<IExplanationDashbo
     this.state = {
       maxK: Math.min(15, Math.ceil(this.countNonzeros(this.props.dataSummary.localExplanations))),
       topK: Math.ceil(this.countNonzeros(this.props.dataSummary.localExplanations) / 2),
-<<<<<<< HEAD
-      radio: "all"
-=======
       radio: RadioKeys.all
->>>>>>> dfb4a24f8e66103b7ebb1dc633066e8d4311ca77
     }
     this.changeRadioButton = this.changeRadioButton.bind(this)
   }
 
   public render() {
     return (
-<<<<<<< HEAD
-      <div style={{backgroundColor: "rgb(220,220,220)", fontFamily: 'Segoe UI'}}>
-         <div className="explainerDashboard">
-          <div className="ms-Grid" dir="ltr">
-            <div className="ms-Grid-row">
-              <div className="ms-Grid-col ms-sm6 ms-md6 ms-lg6">
-                <div style={{marginRight: '5px'}}>
-                  <div style={{marginTop: '5%'}}>
-                    <Slider
-                      min={1}
-                      max={this.state.maxK}
-                      step={1}
-                      defaultValue={(this.state.topK)}
-                      showValue={false}
-                      onChange={(value) => this.setTopK(value)}
-                    />
-                  </div>
-                  <div style={{fontSize: '1.8em', textAlign: 'center', fontStyle: 'italic', fontFamily: 'Segoe UI', color: '#636363', margin: '30px'}}>{this.state.topK.toString() + ' ' + localization.importantWords}</div>
-                  <div style={{marginLeft:'5%', height: '350px'}}>
-                   <BarChart
-                      text={this.props.dataSummary.text}
-                      localExplanations={this.props.dataSummary.localExplanations}
-                      topK={this.state.topK}
-                      radio={this.state.radio}
-                    />
-                    </div>
-                    <div style={{fontFamily: 'Segoe UI', fontSize: '2.0em', fontWeight: "bold", marginBottom:'20px'}}>{localization.label + localization.colon + this.predictClass(this.props.dataSummary.classNames, this.props.dataSummary.prediction)}</div>
-                    <div className="ms-Grid-col ms-sm6 ms-md6 ms-lg6" >
-                      <ChoiceGroup defaultSelectedKey="all" options={options} onChange={this.changeRadioButton} required={true} />
-                    </div>
-                    <div className="ms-Grid-col ms-sm6 ms-md6 ms-lg6" >
-                          {localization.legendText}
-                        </div>
-                </div>
-              </div>
-              <div className="ms-Grid-col ms-sm6 ms-md6 ms-lg6" style={{padding:'20px',}} >
-                <div style={{marginLeft: '5px',}}>
-                  <div style={{borderTopStyle: "solid", borderColor: 'rgb(0,120,212)', backgroundColor: 'white'}} >
-                    <div style={{backgroundColor: 'white', borderStyle: 'groove', borderBlockColor: 'black', borderRadius: '5px', height: '250px', padding:'20px', overflowY: 'auto'}}>
-                      <TextHighlighting
-                        text={this.props.dataSummary.text}
-                        localExplanations={this.props.dataSummary.localExplanations}
-                        topK={this.state.topK}
-                        radio={this.state.radio}
-                      />
-                  </div>
-                  <div style={{margin: '30px 10px 10px 10px'}}>
-                    {localization.featureLegend}
-                  </div>
-                  <span style={{color: 'white', fontWeight: 'bold', backgroundColor: '#0078D4', margin: '10px'}}>{localization.posFeatureImportance}</span>
-                  <br></br>
-                  <span style={{margin: '10px', fontWeight: 'bold', textDecorationLine: 'underline', color: '#0078D4'}}>{localization.negFeatureImportance}</span>                   
-                  </div>
-                </div>
-=======
       <div className='explainerDashboard'>
         <div className = 'sliderWithText' >
           <div className = 'slider'>
@@ -137,7 +73,6 @@ export class ExplanationDashboard extends React.PureComponent<IExplanationDashbo
               </div>
               <div className = 'chartLegend'>
                 {localization.legendText}
->>>>>>> dfb4a24f8e66103b7ebb1dc633066e8d4311ca77
               </div>
             </div>
         </div>
@@ -170,10 +105,7 @@ export class ExplanationDashboard extends React.PureComponent<IExplanationDashbo
   private setTopK(newNumber: number): void {
     this.setState({ topK: newNumber })
   }
-<<<<<<< HEAD
-=======
 
->>>>>>> dfb4a24f8e66103b7ebb1dc633066e8d4311ca77
   private countNonzeros(numArr: number[]): number {
     let counter = 0
     for (const i in numArr) {
@@ -183,10 +115,6 @@ export class ExplanationDashboard extends React.PureComponent<IExplanationDashbo
     }
     return counter
   }
-<<<<<<< HEAD
-  public predictClass(classname, prediciton):string{
-    return classname[Utils.argsort(prediciton)[0]]
-=======
 
   public predictClass(classname, prediction):string{
     return classname[Utils.argsort(prediction)[0]]
@@ -194,9 +122,5 @@ export class ExplanationDashboard extends React.PureComponent<IExplanationDashbo
 
   public changeRadioButton(ev: React.FormEvent<HTMLInputElement>, option: IChoiceGroupOption): void {
     this.setState({ radio: option.key })
->>>>>>> dfb4a24f8e66103b7ebb1dc633066e8d4311ca77
-  }
-  public changeRadioButton(ev: React.FormEvent<HTMLInputElement>, option: IChoiceGroupOption): void {
-    this.setState({radio: option.key})
   }
 }
