2fc6250578f338a86e0df0e83e0bac860174c2f4
Anish Pimpley <anishpimpley@gmail.com>
2020-02-07 13:27:05 -0500

    Anish/cust test pr (#44)
    * fixed size of coef_ to 2D from 1D for binary calssification, to align with multiclass sizes
    
    * added suport for trained models, allowing user to skip training all together
    
    * added scale_to_one parameter for local importances. . Fixed bug with importance handling of binary cls special case
    
    * restored the is_trained flag that was lost in a merge resolution
    
    * resolved PR comments from Ilya and Walter
    
    * flake 8 changes
    
    * more flake8 changes

--- python/interpret_text/classical/classical_text_explainer.py
+++ python/interpret_text/classical/classical_text_explainer.py
@@ -13,16 +13,23 @@ from interpret_community.explanation.explanation import _create_global_explanati
 
 
 class ClassicalTextExplainer:
-    def __init__(self, preprocessor=None, model=None, hyperparam_range=None):
+    def __init__(self, preprocessor=None, model=None,
+                 is_trained=False, hyperparam_range=None):
         self.parsed_sentence = None
         self.word_importances = None
-        self.is_trained = False
         self.model = model
-        self.hyperparam_range = hyperparam_range
-        if self.model is not None and self.hyperparam_range is None:
+        self.is_trained = is_trained
+        if self.model is None and self.is_trained:
             raise Exception(
-                "custom model needs to be supplied with custom hyperparamter range to search over"
+                "is_trained flag can't be set to true, if custom model not provided"
             )
+        self.hyperparam_range = hyperparam_range
+        if self.model is not None:
+            # model is user defined
+            if not self.is_trained and self.hyperparam_range is None:
+                raise Exception(
+                    "custom model needs to be supplied with custom hyperparameter range to search over"
+                )
         self.preprocessor = BOWEncoder() if preprocessor is None else preprocessor
 
     def _encode(self, X_str):
@@ -34,21 +41,20 @@ class ClassicalTextExplainer:
 
     def fit(self, X_str, y_train):
         X_train = self._encode(X_str)
-        if self.model is None:
-            self.model = LogisticRegression()
-            # Hyperparameters were chosen through hyperparamter optimization on MNLI
-            self.hyperparam_range = [ExplainerParams.HYPERPARAM_RANGE]
-        elif self.model is not None and self.hyperparam_range is None:
-            raise Exception(
-                "custom model needs to be supplied with custom hyperparamter range to search over"
+        if self.is_trained is False:
+            if self.model is None:
+                self.model = LogisticRegression()
+                # Hyperparameters were chosen through hyperparamter optimization on MNLI
+                self.hyperparam_range = [ExplainerParams.HYPERPARAM_RANGE]
+            classifier_CV = GridSearchCV(
+                self.model, self.hyperparam_range, cv=3, scoring="accuracy"
             )
-        classifier_CV = GridSearchCV(
-            self.model, self.hyperparam_range, cv=3, scoring="accuracy"
-        )
-        classifier_CV.fit(X_train, y_train)
-        # set model as the best estimator from grid search results
-        self.model = classifier_CV.best_estimator_
-        best_params = classifier_CV.best_params_
+            classifier_CV.fit(X_train, y_train)
+            # set model as the best estimator from grid search results
+            self.model = classifier_CV.best_estimator_
+            best_params = classifier_CV.best_params_
+        else:
+            best_params = self.model.get_params()
 
         # report metrics
         class Encoder:
@@ -70,7 +76,7 @@ class ClassicalTextExplainer:
 
         return [text_model, best_params]
 
-    def explain_local(self, input_text):
+    def explain_local(self, input_text, abs_sum_to_one=False):
         [encoded_text, _] = self.preprocessor.encode_features(
             input_text, needs_fit=False
         )
@@ -79,7 +85,12 @@ class ClassicalTextExplainer:
         encoded_label = encoded_label[0]
         # Obtain the top feature ids for the selected class label
         if hasattr(self.model, "coef_"):
-            encoded_imp = self.model.coef_[encoded_label, :]
+            # when #labels == 2, coef_ returns 1D array
+            label_coefs_all = self.model.coef_
+            if len(self.preprocessor.labelEncoder.classes_) == 2:
+                label_coefs_all = np.vstack((-1 * label_coefs_all,
+                                            label_coefs_all))
+            encoded_imp = label_coefs_all[encoded_label, :]
         elif hasattr(self.model, "feature_importances_"):
             encoded_imp = self.model.feature_importances_
         else:
@@ -87,6 +98,10 @@ class ClassicalTextExplainer:
         decoded_imp, parsed_sentence_list = self.preprocessor.decode_imp(
             encoded_imp, input_text
         )
+
+        if abs_sum_to_one is True:
+            decoded_imp = decoded_imp / (np.sum(np.abs(decoded_imp)))
+
         local_explanantion = _create_local_explanation(
             classification=True,
             text_explanation=True,
