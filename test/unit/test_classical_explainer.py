a8ac56ecf9afb2e12e619c29c94b2b04d472eef0
Janhavi Mahajan <jamahaja@microsoft.com>
2020-03-27 20:47:44 -0400

    Removed spaces from test

--- test/unit/test_classical_explainer.py
+++ test/unit/test_classical_explainer.py
@@ -21,14 +21,12 @@ class TestClassicalExplainer(object):
         :return:
         """
         train_df = get_mnli_test_dataset('train')
-
         X_str = train_df['sentence1']
         ylabels = train_df['genre']
         X_train, X_test, y_train, y_test = train_test_split(X_str, ylabels, train_size=0.8, test_size=0.2)
 
         label_encoder = LabelEncoder()
         y_train = label_encoder.fit_transform(y_train)
-
         explainer = ClassicalTextExplainer()
         classifier, best_params = explainer.fit(X_train, y_train)
         explainer.preprocessor.labelEncoder = label_encoder
